75669fc95bb3d55c996b5ee2bfbf1f3aa575fd58
kubradereli <0kubradereli@gmail.com>
2024-06-26 22:07:08 +0300

    Ana sayfa component partial yapısı kullanılarak yapıldı.

---
+++ Emlak_Projesi/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Emlak_Projesi.Dtos.ProductDtos;
+
+namespace Emlak_Projesi.Repositories.ProductRepository
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+
+        Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync();
+    }
+}
---
+++ Emlak_Projesi/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Emlak_Projesi.Dtos.ProductDtos;
+using Emlak_Projesi.Models.DapperContext;
+
+namespace Emlak_Projesi.Repositories.ProductRepository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+
+        public ProductRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            string query = "Select * From Product";
+            using (var conneciton = _context.CreateConnection())
+            {
+                var values = await conneciton.QueryAsync<ResultProductDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync()
+        {
+            string query = "Select ProductID, Title, Price, City, District, CategoryName From Product inner join Category on Product.ProductCategory=Category.CategoryID";
+            using (var conneciton = _context.CreateConnection())
+            {
+                var values = await conneciton.QueryAsync<ResultProductWithCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
